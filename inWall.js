30ad1f69987a45f3003ea780ebf25f638957fd6e
haydenK-IS <137427701+haydenK-IS@users.noreply.github.com>
2023-06-22 13:01:11 -0400

    Add files via upload

---
+++ inWall.js
@@ -0,0 +1,14 @@
+document.querySelector(
+  '.sendButton').addEventListener(
+    'click',function(){
+      let temp = document.querySelector('.notesInput');
+      document.querySelector('.formRight').innerHTML = temp.value;
+    })
+
+function enter(event){
+  if(event.key === 'Enter')
+  {
+    let temp = document.querySelector('.notesInput');
+      document.querySelector('.formRight').innerHTML = temp.value;
+  }
+}
