74694659478ba2297544f06dc89a21561ac9298f
Hayden Kubit <hkubit@inglett-stubbs.com>
2023-06-23 07:01:27 -0400

    commit

--- inWall.js
+++ inWall.js
@@ -2,6 +2,12 @@ document.querySelector(
   '.sendButton').addEventListener(
     'click',function(){
       let temp = document.querySelector('.notesInput');
-      document.querySelector('.formRight').innerHTML = temp.value();
-      //console.log('click');
+      document.querySelector('.formRight').innerHTML = temp.value;
     })
+
+function enter(event){
+  if(event.key === 'Enter'){
+    let temp = document.querySelector('.notesInput');
+      document.querySelector('.formRight').innerHTML = temp.value;
+  }
+}
